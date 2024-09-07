5b57ef43df6e94182a9be0a7fdc98795f79bacd1
Ben Liu <benliu2345@gmail.com>
2024-09-07 15:45:56 -0700

    finished create user

--- databaseconnection.js
+++ databaseconnection.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
-
+console.log(mongodb_host)
 const MongoClient = require("mongodb").MongoClient;
 const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true`;
+console.log(atlasURI)
 var database = new MongoClient(atlasURI, {useNewUrlParser: true, useUnifiedTopology: true});
 module.exports = {database};
--- main.js
+++ main.js
@@ -54,57 +54,11 @@ app.use(session({
 ));
 
 app.get('/', (req, res) => {
-    res.render("index", {navLinks: navLinks})
+    res.render("index", { navLinks: navLinks })
 });
 
-app.get('/login', (req,res) => {
-    res.render("login", {navLinks: navLinks})
-})
-
-app.post('/loggingin', async (req, res) => {
-    var email = req.body.email;
-    var password = req.body.password;
-
-    const schema = Joi.object(
-        {
-            email: Joi.string().email({ tlds: { allow: false } }).required(),
-            password: Joi.string().max(20).required()
-        });
-
-    const validationResult = schema.validate({email,password});
-    if (validationResult.error != null) {
-        var error = validationResult.error.details[0].message;
-        res.render("login",{error:error,navLinks:navLinks});
-        return;
-    }
-
-    const result = await userCollection.find({ email: email }).project({ username: 1, password: 1, email: 1, _id: 1, user_type: 1 }).toArray();
-
-    console.log(result);
-    if (result.length != 1) {
-        console.log("user not found");
-        res.render("login",{error:"user not found",navLinks:navLinks});
-        return;
-    }
-    if (await bcrypt.compare(password, result[0].password)) {
-        console.log("correct password");
-        req.session.authenticated = true;
-        req.session.username = result[0].username;
-        req.session.cookie.maxAge = expireTime;
-        req.session.user_type = result[0].user_type
-
-        res.redirect('/home');
-        return;
-    }
-    else {
-        console.log("incorrect password");
-        res.render("login",{error:"pssword not found",navLinks: navLinks});
-        return;
-    }
-});
-
-app.get('/signup', (req,res) => {
-    res.render('signup',{navLinks:navLinks})
+app.get('/signup', (req, res) => {
+    res.render('signup', { navLinks: navLinks })
 })
 
 app.post('/submitUser', async (req, res) => {
@@ -123,12 +77,12 @@ app.post('/submitUser', async (req, res) => {
     if (validationResult.error != null) {
         console.log(validationResult.error.details[0].path[0]);
         var error = validationResult.error.details[0].message;
-        res.render("signup",{error: error, navLinks: navLinks});
+        res.render("signup", { error: error, navLinks: navLinks });
         return;
     }
     var hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    await userCollection.insertOne({ username: username, email: email, password: hashedPassword});
+    await userCollection.insertOne({ username: username, email: email, password: hashedPassword });
     console.log("Inserted user");
 
     req.session.username = username;
@@ -137,13 +91,60 @@ app.post('/submitUser', async (req, res) => {
     res.redirect(`/profilesetup`);
 });
 
-app.get('/host', (req,res) => {
-    res.render("host", {navLinks: navLinks})
+app.get('/login', (req, res) => {
+    res.render("login", { navLinks: navLinks })
+})
+
+app.post('/loggingin', async (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    const schema = Joi.object(
+        {
+            email: Joi.string().email({ tlds: { allow: false } }).required(),
+            password: Joi.string().max(20).required()
+        });
+
+    const validationResult = schema.validate({ email, password });
+    if (validationResult.error != null) {
+        var error = validationResult.error.details[0].message;
+        res.render("login", { error: error, navLinks: navLinks });
+        return;
+    }
+
+    const result = await userCollection.find({ email: email }).project({ username: 1, password: 1, email: 1, _id: 1, user_type: 1 }).toArray();
+
+    console.log(result);
+    if (result.length != 1) {
+        console.log("user not found");
+        res.render("login", { error: "user not found", navLinks: navLinks });
+        return;
+    }
+    if (await bcrypt.compare(password, result[0].password)) {
+        console.log("correct password");
+        req.session.authenticated = true;
+        req.session.username = result[0].username;
+        req.session.cookie.maxAge = expireTime;
+        req.session.user_type = result[0].user_type
+
+        res.redirect('/home');
+        return;
+    }
+    else {
+        console.log("incorrect password");
+        res.render("login", { error: "pssword not found", navLinks: navLinks });
+        return;
+    }
+});
+
+
+app.get('/host', (req, res) => {
+    res.render("host", { navLinks: navLinks })
 })
 
 app.post('/host-book-club', (req, res) => {
     const { btitle, bdescription, genres } = req.body;
-    const genreArray = genres.split(','); 
+    const genreArray = genres.split(',');
 
     res.send("Book club hosted successfully.");
 });
